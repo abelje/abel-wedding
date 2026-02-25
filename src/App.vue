04261caf0b84a7ab1b54dc402e6bea7a684f40ee
abelje <abelje2004@gmail.com>
2026-02-25 00:24:54 -0600

    Implemented Hamburger Menu from Class and revamped the homepage with text and images.

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,7 @@ const routes = {
 }
 
 const currentPath = ref(window.location.hash)
+const drawer = ref(false)
 
 window.addEventListener('hashchange', () => {
   currentPath.value = window.location.hash
@@ -20,8 +21,44 @@ const currentView = computed(() => {
 </script>
 
 <template>
-  <a href="#/">Home</a> |
-  <a href="#/about">About</a>
-
-  <component :is="currentView"></component>
+  <v-app>
+    <v-navigation-drawer class="text-white"
+                         v-model="drawer"
+                         color="green-darken-3"
+    >
+      <v-list-item
+          prepend-icon="mdi-home"
+          href="#/"
+          title="Home"
+          @click="drawer = !drawer"
+      ></v-list-item>
+      <v-list-item
+          prepend-icon="mdi-information-variant-circle-outline"
+          href="#/about"
+          title="About Us"
+          @click="drawer = !drawer"
+      ></v-list-item>
+    </v-navigation-drawer>
+    <v-app-bar
+        class="text-white"
+        height="200"
+        color="green-darken-4">
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-app-bar-title class="text-h1 text-center cedarville-cursive-regular">Jon & Kristen</v-app-bar-title>
+    </v-app-bar>
+    <v-main>
+      <component :is="currentView"></component>
+    </v-main>
+    <v-footer app="false">Copyright 2026</v-footer>
+  </v-app>
 </template>
+
+<style scoped>
+.cedarville-cursive-regular {
+  font-family: "Cedarville Cursive", cursive;
+  font-weight: 400;
+  font-style: normal;
+  line-height: 4;
+}
+
+</style>
